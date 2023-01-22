86fc8349103ef7c73207a08d6e7ae5fc63ea1578
Sasha <bezhenar93@internet.ru>
2023-01-22 13:00:17 +0200

    seminar_one

---
+++ seminar_one.java
@@ -0,0 +1,80 @@
+import java.util.Scanner;
+
+public class seminar_one {
+    public static void main(String[] args) {
+        /**
+         * Task1:
+         * Вычислить n-ое треугольного число(сумма чисел от 1 до n),
+         * n! (произведение чисел от 1 до n)
+         */
+
+        // Scanner isScaner = new Scanner(System.in);
+        // System.out.println("Введите число: ");
+        // int x = isScaner.nextInt();
+        // isScaner.close();
+        // int trianINteger = 0;
+        // int factorial = 1;
+        // for (int i = 1; i <= x; i++) {
+        // trianINteger += i;
+        // factorial *= i;
+        // }
+        // System.out.printf("Треугольное число равно: %d\n Факториал равен: %d",
+        // trianINteger, factorial);
+
+        /**
+         * Task2:
+         * Вывести все простые числа от 1 до 1000
+         */
+        // System.out.println("1 - Это единица, она не простая и не составная");
+        // System.out.println("Простые числа:");
+
+        // for (int i = 2; i < 1000; i++) {
+        //     int count = 0;
+        //     for (int j = 1; j <= i; j++) {
+        //         if (i % j == 0) {
+        //             count += 1;
+        //         }
+        //     }
+        //     if (count == 2) {
+        //         System.out.println(i);
+        //     }
+        // }
+
+        /**
+         * Task3:
+         * Реализовать простой калькулятор
+         */
+
+        // double num1;
+        // double num2;
+        // double result;
+        // char op;
+        // Scanner isScanner = new Scanner(System.in);
+        // System.out.print("Введите первое число: ");
+        // num1 = isScanner.nextDouble();
+        // System.out.print("Введите второе число: ");
+        // num2 = isScanner.nextDouble();
+        // System.out.print("Выберите действие: (+, -, *, /): ");
+        // op = isScanner.next().charAt(0);
+        // switch (op) {
+        // case '+':
+        // result = num1 + num2;
+        // break;
+        // case '-':
+        // result = num1 - num2;
+        // break;
+        // case '*':
+        // result = num1 * num2;
+        // break;
+        // case '/':
+        // result = num1 / num2;
+        // break;
+        // default:
+        // System.out.printf("Ошибка, выберите правильное действие ");
+        // return;
+        // }
+        // System.out.print("\nThe result is given as follows:\n");
+        // System.out.printf(num1 + " " + op + " " + num2 + " = " + result);
+
+    }
+}
